a53800ca754d0db6c45b2f87ac91945f82eb99b9
Juliano <julianosoder1989@gmail.com>
2025-08-16 20:20:37 -0300

    feature: Add endpoint to call gemini

--- src/main/java/org/app/config/SecretManager.java
+++ src/main/java/org/app/config/SecretManager.java
@@ -7,28 +7,27 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class SecretManager {
-    private static final Properties properties = new Properties();
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+  private static final Properties properties = new Properties();
+  private static final ObjectMapper objectMapper = new ObjectMapper();
 
+  static {
+    // String path = "/home/soder/Área de trabalho/app/secrets.txt";
+    String path = "/home/soder/Desktop/app/secrets.txt";
+    // String path = "/home/julianos/Documentos/workdir/app/secrets.txt";
 
-    static {
-//        String path = "/home/soder/Área de trabalho/app/secrets.txt";
-//        String path = "/home/soder/Desktop/app/secrets.txt";
-        String path = "/home/julianos/Documentos/workdir/app/secrets.txt";
-
-        try (FileInputStream input = new FileInputStream(path)) {
-            properties.load(input);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load secrets file", e);
-        }
+    try (FileInputStream input = new FileInputStream(path)) {
+      properties.load(input);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to load secrets file", e);
     }
+  }
 
-    public static String getSecret(String key) {
-        return properties.getProperty(key);
-    }
+  public static String getSecret(String key) {
+    return properties.getProperty(key);
+  }
 
-    public static void setMongoURI() {
-        String mongoURI = getSecret("MONGODB_URI");
-        System.setProperty("mongodb.uri", mongoURI);
-    }
+  public static void setMongoURI() {
+    String mongoURI = getSecret("MONGODB_URI");
+    System.setProperty("mongodb.uri", mongoURI);
+  }
 }
